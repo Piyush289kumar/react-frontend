98a922a74f2c3246e029559b7189ca9b847b9d6c
piyush289kumar <piyushraikwar289@gmail.com>
2025-01-19 13:00:46 +0530

    Create Login & Register Page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Link } from "react-router"
 
-function LoginPage() {
+function RegisterPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
@@ -14,10 +14,19 @@ function LoginPage() {
               <form className="p-6 md:p-8">
                 <div className="flex flex-col gap-6">
                   <div className="flex flex-col items-center text-center">
-                    <h1 className="text-2xl font-bold">Welcome back</h1>
+                    <h1 className="text-2xl font-bold">Join Dev Vault</h1>
                     <p className="text-balance text-muted-foreground">
-                      Login to your Acme Inc account
+                      Create your account to get started with our services.
                     </p>
+                  </div>
+                  <div className="grid gap-2">
+                    <Label htmlFor="name">Name</Label>
+                    <Input
+                      id="name"
+                      type="text"
+                      placeholder="Piyush Kumar Raikwar"
+                      required
+                    />
                   </div>
                   <div className="grid gap-2">
                     <Label htmlFor="email">Email</Label>
@@ -41,7 +50,7 @@ function LoginPage() {
                     <Input id="password" type="password" required />
                   </div>
                   <Button type="submit" className="w-full">
-                    Login
+                    Register
                   </Button>
                   <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                     <span className="relative z-10 bg-background px-2 text-muted-foreground">
@@ -78,9 +87,9 @@ function LoginPage() {
                     </Button>
                   </div>
                   <div className="text-center text-sm">
-                    Don&apos;t have an account?{" "}
-                    <Link to={'/register'} className="underline underline-offset-4">
-                      Sign up
+                    Already have an account?{" "}
+                    <Link to={'/login'} className="underline underline-offset-4">
+                      Log In
                     </Link>
                   </div>
                 </div>
@@ -104,4 +113,4 @@ function LoginPage() {
   )
 }
 
-export default LoginPage
+export default RegisterPage
