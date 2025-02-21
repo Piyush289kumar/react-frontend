c234bf3d7b924f161ee5962075640d2dd61226e4
Piyush289kumar <piyushraikwar289@gmail.com>
2025-02-21 23:43:53 +0530

    Hero Section Build

--- src/pages/LandingPages/LandingHomePage.tsx
+++ src/pages/LandingPages/LandingHomePage.tsx
@@ -1,16 +1,12 @@
+import { WatchReel } from '@/components/landing/watch-reel';
 import HomeHeroImg from '../../assets/kiornos_assets/HomeHeroImg.png';
 
 function LandingHomePage() {
     return (
         <div className="">
-            <div className="relative">
-                {/* Move Image to the Top */}
-                <img src={HomeHeroImg} alt="Hero Home Image" className="w-full" />
-
-                {/* Blur Effect at the Bottom Blending into White */}
+            <div className="relative">                
+                <img src={HomeHeroImg} alt="Hero Home Image" className="w-full" />            
                 <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-white via-white/70 to-transparent backdrop-blur-lg"></div>
-
-                {/* Other Content Below the Image */}
                 <div className="text-center text-zinc-950 -mt-52 flex flex-col justify- gap-y-5">
                     <h1 className="text-8xl font-bold">Create Different.</h1>
                     <p className="text-lg">We build brands and digital experiences that redefine industries.</p>
@@ -21,6 +17,7 @@ function LandingHomePage() {
 
                 </div>
             </div>
+            <WatchReel/>
         </div>
     )
 }
